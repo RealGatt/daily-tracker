8a1b469203dc158333cd0844c8c41daf2b3bdfca
Gatt <1311244+RealGatt@users.noreply.github.com>
2024-01-05 21:18:37 +1100

    dvh

--- src/components/custom-ui/footer.tsx
+++ src/components/custom-ui/footer.tsx
@@ -5,7 +5,7 @@ import ControlDrawer from "../dialog/control-drawer";
 export default async function Footer() {
 	return (
 		<footer className="border-t-2 border-gray-200 dark:border-gray-800 flex flex-row gap-4 px-4 py-1 place-items-center self-center">
-			<div className="w-full flex flex-col place-items-center self-center max-w-6xl  h-full">
+			<div className="w-full flex flex-col place-items-center self-center max-w-6xl h-full">
 				<p className="text-center">
 					Developed with ❤️ by{" "}
 					<a
